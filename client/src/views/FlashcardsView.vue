cb8e346373262f6a7da5751a9db2ca100284c0f6
@theship <juburdekin@gmail.com, julee@abevy.com>
2024-04-02 11:33:53 -0700

    replaced FlashcardsView.vue

---
+++ client/src/views/FlashcardsView.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="flashcards">
+    <h1>This is the flashcards page</h1>
+
+    <TheFlashcard
+      v-for="flashcard in flashcards"
+      :key="flashcard.id"
+      :question="flashcard.question"
+      :answer="flashcard.answer"></TheFlashcard>
+
+  </div> 
+</template>
+
+<script>
+export default {
+  props: ['question', 'answer'],
+  data() {
+    return {
+      flipped: false,
+    };
+  },
+  methods: {
+    flipCard() {
+      this.flipped = !this.flipped;
+    },
+  },
+};
+</script>
+
